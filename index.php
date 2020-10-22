f06a113223f2550ec16f61dace274ff2155ce155
Andrea Contestabile <andreacontest@gmail.com>
2020-10-22 16:40:16 +0200

    Stile pagina

--- index.php
+++ index.php
@@ -24,13 +24,16 @@ $andrea = new User("Seven897", "andreacontest@gmail.com", "password12345");
 $andrea->active = true;
 
 $mario = new User("Teemo123", "mario.rossi@gmail.com", "yasuo12345");
-$mario->active = true;
 
 $silvia = new User("Silver91", "silvia.rossetti@yahoo.com", "parolasegreta765");
 $silvia->name = "Silvia";
 $silvia->lastname = "Rossetti";
+$silvia->active = true;
 
-$users = [$andrea, $mario, $silvia];
+$lucia = new User("Lux77", "lucia.bertini@libero.it", "demacia404");
+$lucia->active = true;
+
+$users = [$andrea, $mario, $silvia, $lucia];
 
 ?>
 
@@ -40,16 +43,15 @@ $users = [$andrea, $mario, $silvia];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
   <title>PHP OOP</title>
 </head>
 <body>
   <div class="container">
+    <h1>Database Segreto</h1>
     <table>
       <thead>
-        <tr class="table-title">
-          <th>Database Segreto</th>
-        </tr>
         <tr>
           <th class="table-heading">Full Name</th>
           <th class="table-heading">Username</th>
@@ -65,13 +67,15 @@ $users = [$andrea, $mario, $silvia];
               <td><?php echo $user->username ?></td>
               <td><?php echo $user->email ?></td>
               <td><?php echo $user->password ?></td>
-              <td>
-                <?php if($user->active == true): ?>
+              <?php if($user->active == true): ?>
+                <td class="green">
                   <i class="fas fa-check"></i>
-                <?php else: ?>
+                </td>
+              <?php else: ?>
+                <td class="red">
                   <i class="fas fa-times"></i>
-                <?php endif ?>
-              </td>
+                </td>
+              <?php endif ?>
           </tr>
         <?php endforeach ?>
       </tbody>
